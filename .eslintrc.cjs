83be2279c74a19e13468e70f7606fee203d4f37d
Megan Groves <grovem2@mcmaster.ca>
2026-01-30 21:46:21 -0500

    Ignoring the ESLint config file in linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: ["./tsconfig.json"],
+    tsconfigRootDir: __dirname
+  },
+  plugins: ["@typescript-eslint"],
+  extends: ["standard-with-typescript"],
+  env: {
+    node: true,
+    es2022: true
+  },
+  ignorePatterns: ["node_modules", "dist", "build", ".eslintrc.cjs"]
+};
