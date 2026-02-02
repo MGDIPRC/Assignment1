0df6a6e256d34e54aa7a8e0d684077db4a1e676c
Megan Groves <grovem2@mcmaster.ca>
2026-02-01 22:12:16 -0500

    third time is the charm. Resolving prettier and eslint rule conflicts

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,10 +6,18 @@ module.exports = {
     tsconfigRootDir: __dirname,
   },
   plugins: ['@typescript-eslint'],
-  extends: ['standard-with-typescript'],
+  extends: [
+    'standard-with-typescript',
+    'prettier', // ⬅️ MUST be last
+  ],
   env: {
     node: true,
     es2022: true,
   },
   ignorePatterns: ['node_modules', 'dist', 'build', '.eslintrc.cjs'],
+  rules: {
+    '@typescript-eslint/space-before-function-paren': 'off',
+    '@typescript-eslint/member-delimiter-style': 'off',
+    '@typescript-eslint/comma-dangle': 'off',
+  },
 }
